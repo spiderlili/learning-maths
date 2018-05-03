c099bb4822402567e676cf7207c09a59d29673c3
Jing Chun Tan <jtan1993@gmail.com>
2018-05-03 23:33:38 +0100

    Update MoveStraightLine.cs

--- MoveStraightLine.cs
+++ MoveStraightLine.cs
@@ -2,15 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//moving objects without physics
+//moving objects without physics - most basic code for moving character in a straight line with consistent speed
 
 public class MoveStraightLine : MonoBehaviour{
   
-  //humanoid character - no floating in y direction
-  Vector3 goal = new Vector3(5,0,4); 
+  //humanoid character - no floating in y direction, mobe along from any location
+  public Vector3 goal = new Vector3(5,0,4); 
+  public float speed = 0.1f;
 
   void Start(){
-  //add the goal vector onto the transform vector of the character
-    this.transform.Translate(goal);
+    //moving aong at 10% of goal each time
+    goal = goal * 0.01f;
+  }
+  
+  //everytime Update runs: not necessarily at exactly the same time the last update happened => inconsistencies
+  //any movement on characters - especially when adding character cameras for following things around: 
+  //make it happen after all the physics has been calculated => LateUpdate() > Update()
+  
+  void LateUpdate(){
+    //add the goal vector of unit length 1 onto the transform vector of the character - move at the same rate for different goal
+    //smooth out and allow for the difference in the update running
+    
+    this.transform.Translate(goal.normalized * speed * Time.deltaTime);
   }
 }
