2d741d7d4dbe38c992d6746767c85ac692b0c4b2
Jing Chun Tan <jtan1993@gmail.com>
2018-05-28 11:08:53 +0100

    Update MoveLocalSlerp.cs

--- MoveLocalSlerp.cs
+++ MoveLocalSlerp.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//make the character turn slowly towards the target rather than snapping 
+//using slerp(spherical linear interpolation) between the angle of the current facing vector and the angle of the goal facing vector over time
+
+
 public class MoveLocalSlerp : MonoBehaviour {
 
 	public Transform goal;
 	float speed = 0.5f;
 	float accuracy = 1.0f;
+	float rotSpeed = 0.4f;
 
 	void Start () {
 		
@@ -15,7 +20,18 @@ public class MoveLocalSlerp : MonoBehaviour {
 
 	void LateUpdate () {
 		Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
-		this.transform.LookAt(lookAtGoal);
+		
+		//work out the direction to face towards the goal vector
+		Vector3 direction = lookAtGoal - this.transform.position;
+		
+		//snap to the right direction 
+		//this.transform.LookAt(lookAtGoal);
+		
+		//using quarternion - a complex data structure holding a rotation - to do fantastic things
+		//turning an angle from facing in one direction to another: takes the current character rotation, perform LookRotation on direction vector
+		//LookRotation gives the rotation that needs to occur to be facing in the direction
+		
+		this.transform.rotation = Quarternion.Slerp(this.transform.rotation, Quarternion.LookRotation(direction), rotSpeed*Time.deltaTime);
 		if(Vector3.Distance(transform.position,lookAtGoal) > accuracy){
 			this.transform.Translate(0,0,speed*Time.deltaTime);
 		}
