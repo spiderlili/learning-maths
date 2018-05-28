f67be9c86e3e6b6091130424ad6118c351ee6767
Jing Chun Tan <jtan1993@gmail.com>
2018-05-28 10:39:16 +0100

    Update MoveLocal.cs

--- MoveLocal.cs
+++ MoveLocal.cs
@@ -9,7 +9,7 @@ public class MoveLocal : MonoBehaviour {
 
 	public Transform goal;
 	float speed = 0.5f; //adjust according to the animation
-	float accuracy = 1.0f;
+	float accuracy = 1.0f; //get rid of jittering when the character reaches the goal
 
 	void Start () {
 		
@@ -19,14 +19,17 @@ public class MoveLocal : MonoBehaviour {
 		//change the direction to look for the enemy
 		//the character performs a LookAt at the a point in the same Y as the character, but the same X anz Z as the object
 		//create a new goal position to look at(lock the y axis to character height)
+		
 		this.transform.LookAt(lookAtGoal);
 		Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
 		
 		//rotating and orientating the model towards that direction regardless of where in 3d space the goal is positioned 
 		//so the z axis is pointing towards the position of the goal
+		//test the distance between current location and the goal position - stop if it's smaller than accuracy offset
 
-		if(Vector3.Distance(transform.position,lookAtGoal) > accuracy)
+		if(Vector3.Distance(transform.position,lookAtGoal) > accuracy){
 			//push the enemy along its z axis
 			this.transform.Translate(0,0,speed*Time.deltaTime);
+		}
 	}
 }
